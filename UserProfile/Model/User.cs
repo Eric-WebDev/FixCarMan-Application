009141b17dd58021b62e815fad970146182a5c27
arek512 <A_lesica@outlook.com>
2020-11-11 16:10:13 +0000

    Add user model class

---
+++ UserProfile/Model/User.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UserProfile.Model
+{
+    public class User
+    {
+        [Required]
+        public Guid UserId { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        public string CompanyName { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string PhoneNumber { get; set; }
+        [Required]
+        public string URL { get; set; }
+        [Required]
+        public DateTime? Birthday { get; set; }
+        [Required]
+        public string Street { get; set; }
+        [Required]
+        public string City { get; set; }
+        [Required]
+        public string State { get; set; }
+        [Required]
+        public string Country { get; set; }
+        [Required]
+        public string ZipCode { get; set; }
+        [Required]
+        public bool IsUserGarage { get; set; }
+    }
+}
